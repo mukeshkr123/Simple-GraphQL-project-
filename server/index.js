74218e35025f6187a21498963cbc5de37bedd706
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-17 11:44:50 +0530

    added user type

--- server/index.js
+++ server/index.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
-const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLString,
+} = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
-
 const PORT = 4040;
 const userData = require("./MOCK_DATA.json");
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUser,
+  },
+});
+
+const Mutation = "mutation";
+
+const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
 app.use(
   "/graphql",
