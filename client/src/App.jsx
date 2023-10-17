41a9897ab48c1a5443a7024630c62a1ce66fbe8a
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-17 12:46:19 +0530

    Set up Apollo Client for GraphQl in React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkErrors }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`GraphQL error: ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:4040/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ApolloProvider client={client}>
+      {/* Your app content goes here */}
+      App
+    </ApolloProvider>
+  );
 };
 
 export default App;
