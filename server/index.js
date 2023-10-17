ef3874d2c8307049458b6502326cefda0733de5b
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-17 11:56:31 +0530

    added createUser mutation

--- server/index.js
+++ server/index.js
@@ -37,7 +37,30 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
-const Mutation = "mutation";
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
 
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
